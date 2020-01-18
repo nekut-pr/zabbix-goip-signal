89b3733616ce68c49a086c90fc65ac062a43ec1c
Nekut-02 <38611552+Nekut-02@users.noreply.github.com>
2020-01-19 00:08:43 +0300

    Update perl.pl

--- perl.pl
+++ perl.pl
@@ -4,24 +4,24 @@ use strict;
 use warnings;
 use HTML::Parser;
 
-my $file_curle = "/usr/scripts/go-ip/curl.html";
-my $file_output = "/usr/scripts/go-ip/output.txt";
+my $file_curle = "путь к файлу для сохранения выхлопа сurl";
+my $file_output = "путь к файлу для хранения цифры уровня сигнала";
 
-system("curl -s -u admin:admin 'http://192.168.33.250/default/en_US/status.html' | grep l1_gsm_signal >> $file_curle");
+system("curl -s -u admin:admin 'http://ip_address_go-ip/default/en_US/status.html' | grep l1_gsm_signal >> $file_curle");
 
-open(my $in, "<", $file_curle)                              # открываем файл на чтение
+open(my $in, "<", $file_curle)                             
     or die "Не могу открыть файл на чтение";
-        my @array = <$in>;                                  # считываем строки файла в массив
-close($in);                                                 # закрываем файл
+        my @array = <$in>;                                  
+close($in);                                                 
 
-open(my $out, ">", $file_curle)                             # открываем файл на запись
+open(my $out, ">", $file_curle)                             
     or die "Не могу открыть файл на запись";
-        foreach my $x (@array) {                            # последовательно перебираем все элементы массива
-            $x =~ s/\s//g;                                  # убераем пробелы
-            $x =~ s/&nbsp;//g;                              # делаем замену   
-        print $out $x;                                      # выводим в файл
+        foreach my $x (@array) {                            
+            $x =~ s/\s//g;                                  
+            $x =~ s/&nbsp;//g;                                 
+        print $out $x;                                     
     }
-close($out);                                                # закрываем файл
+close($out);                                               
 
 my $p = HTML::Parser->new( api_version => 3, text_h => [\&text, "event,text" ]);
     $p->parse_file($file_curle);
@@ -32,4 +32,4 @@ sub text {
         print $fh $text or die "Не могу записать данные в $file_output\n";
 }
 
-unlink $file_curle; # удаляем файл
+unlink $file_curle;
